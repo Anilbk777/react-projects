2b50d2ff19fb05517ee1851f03814c6186937745
Anilbk777 <anilbk775@gmail.com>
2026-04-22 18:13:33 +0545

    counter using hooks(useState)

--- 01vitereact/src/Component.jsx
+++ 01vitereact/src/Component.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 function Component1() {
-  return <h1>Hello from component one.</h1>;
+    let [counter, setCounter] = useState(0)
+
+    let AddCount = () => {
+        if (counter <= 19) {
+            setCounter((counter) => counter + 1)
+        }
+
+    }
+
+    let SubCount = () => {
+        if (counter >=1) setCounter((counter) => counter - 1);
+        
+    }
+
+    return (
+      <>
+        <h2>Counter</h2>
+        <span>value: {counter}</span>
+
+        <button onClick={AddCount}>Increase count</button>
+        <button onClick={SubCount}>Decrease count</button>
+      </>
+    );
 }
 
 export default Component1;
