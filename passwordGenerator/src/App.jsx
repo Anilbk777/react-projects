97ccc5ed9d0e5a9ba1173ca3aff317bdb81ea816
Anilbk777 <anilbk775@gmail.com>
2026-04-25 20:19:05 +0545

    css added

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Password Generator</h1>
-    
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      <div className="flex justify-center m-4">
+        <div className="container bg-sky-500/50 rounded-lg w-120  flex flex-col items-center ">
+          <div className=" inline-block m-5 ">
+            <input
+              className=" bg-white text-black p-2 border-none outline-none focus:ring-0 rounded-l-lg w-90"
+              type="text"
+              placeholder="abcde"
+            />
+            <button className="bg-blue-600 p-2 rounded-r-lg">copy</button>
+          </div>
+          <div className="flex gap-2 text-red-600">
+            <input
+              type="range"
+              className="border-none outline-none focus:outline-none focus:ring-0"
+            />
+            length(0)
+            <input type="checkbox" />
+            Numbers
+            <input type="checkbox" /> Characters
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
