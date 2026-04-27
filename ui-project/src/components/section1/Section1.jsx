c3dc9c0e9469e8e5739b44a81b3b9be742db8415
Anilbk777 <anilbk775@gmail.com>
2026-04-27 08:31:51 +0545

    section1 top nav bar added

---
+++ ui-project/src/components/section1/Section1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Top from './Top'
+import Page1Content from './Page1Content'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full '>
+      <Top />
+      <Page1Content/>
+    </div>
+  )
+}
+
+export default Section1
---
+++ ui-project/src/components/section1/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className='flex items-center justify-between py-8 px-16'>
+      <h4 className='bg-black text-white py-2 px-6 rounded-full'>TARGET AUDIENCE</h4>
+      <button className='bg-gray-200 py-2 px-6 uppercase rounded-full tracking-widest text-sm'>digital Banking Platform</button>
+    </div>
+  );
+}
+
+export default Top
