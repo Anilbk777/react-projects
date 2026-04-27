b85925a9f74fbb743c02847643c5928c81553d46
Anilbk777 <anilbk775@gmail.com>
2026-04-27 14:57:41 +0545

    onchange button added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,35 +5,50 @@ function App() {
 
   let [color, setColor] = useState("black")
 
+  const typedValue = (val) => {
+    console.log(val)
+  }
   return (
-    <div
-      className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
-    >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button
-            onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "red" }}
-          >
-            Red
-          </button>
-          <button
-            onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "green" }}
-          >
-            Green
-          </button>
-          <button
-            onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "blue" }}
-          >
-            blue
-          </button>
-        </div>
+    // <div
+    //   className="w-full h-screen duration-200"
+    //   style={{ backgroundColor: color }}
+    // >
+    //   <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+    //     <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+    //       <button
+    //         onClick={() => setColor("red")}
+    //         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+    //         style={{ backgroundColor: "red" }}
+    //       >
+    //         Red
+    //       </button>
+    //       <button
+    //         onClick={() => setColor("green")}
+    //         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+    //         style={{ backgroundColor: "green" }}
+    //       >
+    //         Green
+    //       </button>
+    //       <button
+    //         onClick={() => setColor("blue")}
+    //         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+    //         style={{ backgroundColor: "blue" }}
+    //       >
+    //         blue
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="h-screen w-screen">
+      <div>
+        <input
+          onChange={(events)=>{
+          typedValue(events.target.value)
+          }}
+          className="border border-black-800 rounded-md px-4 py-2 focus:outline-none focus:border-blue-800 m-5"
+          type="text"
+          placeholder="Enter Your Name"
+        />
       </div>
     </div>
   );
