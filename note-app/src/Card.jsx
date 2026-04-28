4931305de56631bd7c43542169d0f56ad284014f
Anilbk777 <anilbk775@gmail.com>
2026-04-29 00:51:09 +0545

    card display data successfully

--- note-app/src/Card.jsx
+++ note-app/src/Card.jsx
@@ -1,21 +1,11 @@
-import React from 'react'
-
-const Card = () => {
+const Card = ({ elem }) => {
   return (
-    <div className="border-l-2">
-      <h1 className="text-4xl font-bold m-10">Your Recent Notes</h1>
-      <div className="flex flex-wrap gap-5 ml-10">
-        <div className="bg-white h-60 w-50 rounded overflow-y-auto">
-          <div className="text-1xl text-black p-4 border-b-2 ">
-          </div>
-          <p className="text-black p-4">
-          </p>
-        </div>
-        <div className="bg-white h-60 w-50 rounded"></div>
-        <div className="bg-white h-60 w-50 rounded"></div>
-      </div>
+    <div className="bg-white h-60 w-50 rounded overflow-y-auto mr-2">
+      <div className="text-xl text-black p-4 border-b-2">{elem.title}</div>
+
+      <p className="text-black p-4">{elem.details}</p>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
--- note-app/src/Form.jsx
+++ note-app/src/Form.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
-const Form = () => {
 
-    function submitHandler(dets) {
-        dets.preventDefault()
-        console.log("form submission prevented!")
-    }
+const Form = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const [task, setTask] = useState([]);
+
+  function submitHandler(dets) {
+    dets.preventDefault();
+
+    const newTask = {
+      title,
+      details,
+    };
+    setTask((prev) => [...prev, newTask]);
+    console.log(task);
+    setTitle("");
+    setDetails("");
+  }
   return (
     <div className="flex justify-center">
       <form
@@ -18,18 +31,29 @@ const Form = () => {
           type="text"
           placeholder="Enter Notes Heading"
           className="px-5 py-2  border-2 rounded-2xl font-medium"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
         <textarea
           type="text"
           placeholder="Enter Details"
           className="px-5 py-2 h-20 border-2 rounded-2xl font-medium"
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
         />
 
         <button className="bg-white text-black px-5 py-2 rounded-2xl font-medium">
           Add Note
         </button>
       </form>
-      <Card />
+      {
+        task.map((elem, index) => {
+          return <Card key={index} elem={elem} />;
+        })}
     </div>
   );
 };
