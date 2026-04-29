ea5cdc4f41907c36c96fe0c9c97ef8289b42fa78
Anilbk777 <anilbk775@gmail.com>
2026-04-29 11:31:34 +0545

    api

---
+++ note-app/src/Api.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Api = () => {
+    const getData = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const data = await response.json()
+        console.log(data)
+    }
+  return (
+    <div>
+      <button
+        onClick={getData}
+        className="border-2 rounded px-4 py-2 text-2xl bg-blue-500 transition-transform active:scale-95"
+      >
+        Get data
+      </button>
+    </div>
+  );
+}
+
+export default Api
--- note-app/src/App.jsx
+++ note-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from './Form';
+import Api from './Api';
 
 const App = () => {
   return (
     <div className="min-h-screen w-full bg-black text-white ">
-      <h1 className="text-center text-4xl font-bold p-4 border-2 mx-150 rounded-3xl mb-12">Note App</h1>
-      <Form />
+    {/* <h1 className="text-center text-4xl font-bold p-4 border-2 mx-150 rounded-3xl mb-12">Note App</h1>
+     <Form /> */}
+    <Api/>
     </div>
   );
 }
