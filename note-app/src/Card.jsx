5cbca52984facece091091912a2f56241ba1d3bb
Anilbk777 <anilbk775@gmail.com>
2026-04-29 11:01:06 +0545

    Form updated

--- note-app/src/Card.jsx
+++ note-app/src/Card.jsx
@@ -1,9 +1,9 @@
 const Card = ({ elem }) => {
   return (
     <div className="bg-white h-60 w-50 rounded overflow-y-auto mr-2 ">
-      <div className="text-xl text-black p-4 border-b-2">{elem.title}</div>
+      <div className="text-xl text-black p-4 border-b-2">{elem.heading}</div>
 
-      <p className="text-black p-4">{elem.details}</p>
+      <p className="text-black p-4">{elem.detail}</p>
     </div>
   );
 };
--- note-app/src/Form.jsx
+++ note-app/src/Form.jsx
@@ -2,60 +2,65 @@ import React, { useState } from "react";
 import Card from "./Card";
 
 const Form = () => {
-  const [title, setTitle] = useState("");
-  const [details, setDetails] = useState("");
+  const [formData, setFormData] = useState({
+    heading: "",
+    detail: "",
+  });
 
   const [task, setTask] = useState([]);
+
+  function changeHandler(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
 
   function submitHandler(dets) {
     dets.preventDefault();
 
     const newTask = {
-      title,
-      details,
+      heading: formData.heading,
+      detail: formData.detail,
     };
+
     setTask((prev) => [...prev, newTask]);
-    console.log(task);
-    setTitle("");
-    setDetails("");
+
+    setFormData({ heading: "", detail: "" });
   }
   return (
     <div className="flex justify-center">
       <form
-        onSubmit={(event) => {
-          submitHandler(event);
-        }}
+        onSubmit={submitHandler}
         className="flex flex-col gap-5 p-10 w-[30vw]"
       >
-              <input
-                  required
+        <input
+          required
+          name="heading"
           type="text"
           placeholder="Enter Notes Heading"
           className="px-5 py-2  border-2 rounded-2xl font-medium"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
+          value={formData.heading}
+          onChange={changeHandler}
         />
-              <textarea
-                  required
+        <textarea
+          required
+          name="detail"
           type="text"
           placeholder="Enter Details"
           className="px-5 py-2 h-20 border-2 rounded-2xl font-medium"
-          value={details}
-          onChange={(e) => {
-            setDetails(e.target.value);
-          }}
+          value={formData.detail}
+          onChange={changeHandler}
         />
 
         <button className="bg-white text-black px-5 py-2 rounded-2xl font-medium">
           Add Note
         </button>
       </form>
-      {task &&
-        task.map((elem, index) => {
-          return <Card key={index} elem={elem} />;
-        })}
+      {task.map((elem, index) => (
+        <Card key={index} elem={elem} />
+      ))}
     </div>
   );
 };
