df1a7626b26db5d7de15c096252b8550e19761c4
Anilbk777 <anilbk775@gmail.com>
2026-05-02 20:12:31 +0545

    Log out button

--- EmployeeManagementSystem/src/Components/Other/Header.jsx
+++ EmployeeManagementSystem/src/Components/Other/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 className='text-2xl font-medium'>
         Hello <br /> <span className='text-3xl font-semibold'>Anil✌️</span>
       </h1>
-      <button>Log out</button>
+      <button className='bg-red-600 text-lg px-4 py-2 rounded-sm font-medium'>Log Out</button>
     </div>
   );
 }
