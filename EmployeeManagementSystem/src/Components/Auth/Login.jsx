cc7913a81ae0b0124ce001134e5b89d3eaef6de8
Anilbk777 <anilbk775@gmail.com>
2026-05-02 19:35:31 +0545

    form submit and button effect added

--- EmployeeManagementSystem/src/Components/Auth/Login.jsx
+++ EmployeeManagementSystem/src/Components/Auth/Login.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Login() {
+  const formRef = useRef(null);
+  const buttonClick = () => {
+    formRef.current.requestSubmit();
+  };
   return (
     <div className=" h-screen w-full flex flex-col items-center justify-center gap-5 ">
       <h1 className="text-2xl font-bold">Log In</h1>
       <div>
         <div>
-          <form className="flex flex-col gap-5 mb-5">
+          <form
+            ref={formRef}
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            className="flex flex-col gap-5 mb-5"
+          >
             <input
+              required
               type="email"
               name="email"
               placeholder="Email"
               className="border-2 outline-none border-emerald-600 rounded-full px-4 py-2 w-[20vw] text-xl placeholder:text-gray-400"
             />
             <input
+              required
               type="password"
               name="password"
               placeholder="Password"
@@ -30,8 +42,8 @@ function Login() {
           </div>
           <div className="text-gray-400">Forget password</div>
         </div>
-        <div className=" bg-emerald-600 rounded-full px-4 py-2 w-[20vw] text-center text-xl font-medium">
-          <button>Log In</button>
+        <div className=" bg-emerald-600 rounded-full px-4 py-2 w-[20vw] text-center text-xl font-medium hover:bg-emerald-700 active:scale-95">
+          <button onClick={buttonClick}>Log In</button>
         </div>
       </div>
     </div>
