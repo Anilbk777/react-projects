eb03556cdbfc208f4f2829983d79a65d38e5ac2f
Anilbk777 <anilbk775@gmail.com>
2026-05-02 20:36:35 +0545

    tasklist layout added

--- EmployeeManagementSystem/src/Components/Dashboard/EmployeeDashboard.jsx
+++ EmployeeManagementSystem/src/Components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Other/Header'
+import TaskListNumbers from '../Other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div>
           <div className='h-screen p-10 bg-[#1c1c1c] '>
-              <Header/>
+              <Header />
+              <TaskListNumbers />
+              <TaskList/>
       </div>
     </div>
   )
---
+++ EmployeeManagementSystem/src/Components/TaskList/TaskList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div id="tasklist" className="flex gap-5 overflow-x-auto flex-nowrap  h-[55%]  mt-10 py-5">
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+      <div className="shrink-0 bg-yellow-300 h-full w-[300px] rounded-xl"></div>
+    </div>
+  );
+};
+
+export default TaskList;
