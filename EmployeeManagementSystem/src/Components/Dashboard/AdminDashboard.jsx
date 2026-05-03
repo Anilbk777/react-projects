8c10870caabaafc21f044616ece24d48cb3dab5d
Anilbk777 <anilbk775@gmail.com>
2026-05-03 19:56:25 +0545

    create task card added

--- EmployeeManagementSystem/src/Components/Dashboard/AdminDashboard.jsx
+++ EmployeeManagementSystem/src/Components/Dashboard/AdminDashboard.jsx
@@ -4,15 +4,15 @@ const AdminDashboard = () => {
   return (
     <div className="min-h-screen w-full p-10">
       <Header />
-      <div className="border-2 border-amber-500 mr-[20vw] ml-[20vw]">
+      <div className=" flex justify-center flex-col ml-[34vw] mr-[26vw] p-4 rounded-md bg-gray-900">
         <h1 className="text-center text-3xl font-bold mb-4">Create Task</h1>
-        <form className="flex flex-col items-center justify-center ml-[37vw] mr-[35vw]">
+        <form className="flex flex-col items-center justify-center ">
           <div>
             <h3 className="text-lg font-small mb-1">Task Title</h3>
             <input
               type="text"
               placeholder="Make a UI design"
-              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
+              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-md mb-5 placeholder:font-light"
             />
           </div>
           <div>
@@ -21,21 +21,21 @@ const AdminDashboard = () => {
               name=""
               id=""
               placeholder="Detail description of task (Max 500 words)"
-              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
+              className="bg-gray-600 resize-none w-[25vw] px-4 py-1 outline-none  rounded-md mb-5 placeholder:font-light"
             ></textarea>
           </div>
           <div>
             <h3 className="text-lg font-small mb-1">Date</h3>
             <input
               type="date"
-              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
+              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-md mb-5 placeholder:font-light"
             />
           </div>
           <div>
             <h3 className="text-lg font-small mb-1">Assign To</h3>
             <input
               type="text"
-              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
+              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-md mb-5 placeholder:font-light"
             />
           </div>
           <div>
@@ -43,10 +43,10 @@ const AdminDashboard = () => {
             <input
               type="text"
               placeholder="Design, Development, etc..."
-              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
+              className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-md mb-5 placeholder:font-light"
             />
           </div>
-          <button className="bg-black text-white font-medium text-lg border-2 px-4 py-1 rounded-xl w-[25vw] mb-5">
+          <button className="bg-black text-white font-bold text-lg  px-4 py-2 rounded-md w-[25vw] mb-5">
             Create Task
           </button>
         </form>
