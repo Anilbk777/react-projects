16e11f427fad0f9b0dd9287e2ec6f3d85b11a6af
Anilbk777 <anilbk775@gmail.com>
2026-05-03 19:32:12 +0545

    button added

--- EmployeeManagementSystem/src/Components/Dashboard/AdminDashboard.jsx
+++ EmployeeManagementSystem/src/Components/Dashboard/AdminDashboard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Header from "../Other/Header";
 const AdminDashboard = () => {
   return (
-    <div className="h-screen w-full p-10">
+    <div className="min-h-screen w-full p-10">
       <Header />
-      <div className="border-2 border-amber-500">
+      <div className="border-2 border-amber-500 mr-[20vw] ml-[20vw]">
         <h1 className="text-center text-3xl font-bold mb-4">Create Task</h1>
         <form className="flex flex-col items-center justify-center ml-[37vw] mr-[35vw]">
           <div>
@@ -46,7 +46,9 @@ const AdminDashboard = () => {
               className="bg-gray-600 w-[25vw] px-4 py-1 outline-none rounded-xl mb-5 placeholder:font-light"
             />
           </div>
-          <button>Create Task</button>
+          <button className="bg-black text-white font-medium text-lg border-2 px-4 py-1 rounded-xl w-[25vw] mb-5">
+            Create Task
+          </button>
         </form>
       </div>
     </div>
