cd985dd00778aadb77106ad4da300bed03c0fcd4
Anilbk777 <anilbk775@gmail.com>
2026-05-04 15:15:22 +0545

    task list number updated

--- EmployeeManagementSystem/src/Components/Dashboard/EmployeeDashboard.jsx
+++ EmployeeManagementSystem/src/Components/Dashboard/EmployeeDashboard.jsx
@@ -3,16 +3,16 @@ import Header from '../Other/Header'
 import TaskListNumbers from '../Other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({ data }) => {
   return (
     <div>
-          <div className='h-screen p-10 bg-[#1c1c1c] '>
-              <Header />
-              <TaskListNumbers />
-              <TaskList/>
+      <div className="h-screen p-10 bg-[#1c1c1c] ">
+        <Header data={data} />
+        <TaskListNumbers data={data} />
+        <TaskList data={data} />
       </div>
     </div>
-  )
+  );
 }
 
 export default EmployeeDashboard
