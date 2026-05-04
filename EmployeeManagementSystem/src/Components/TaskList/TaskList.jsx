7e9ede4abd1b8a789f347824c0caabadbebaf811
Anilbk777 <anilbk775@gmail.com>
2026-05-04 15:59:47 +0545

    array of task list added

--- EmployeeManagementSystem/src/Components/TaskList/TaskList.jsx
+++ EmployeeManagementSystem/src/Components/TaskList/TaskList.jsx
@@ -4,16 +4,22 @@ import CompletedTask from "./CompletedTask";
 import FailedTask from "./FailedTask";
 import NewTask from "./NewTask";
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
   return (
     <div
       id="tasklist"
-      className="flex gap-5 overflow-x-auto flex-nowrap  h-[55%]  mt-10 py-5">
-      <NewTask/>
-      <CompletedTask/>
+      className="flex gap-5 overflow-x-auto flex-nowrap  h-[55%]  mt-10 py-5"
+    >
+      {/* <NewTask />
+      <CompletedTask />
       <AcceptedTask />
-      <FailedTask/>
-      
+      <FailedTask /> */}
+      {data.tasks.map((elem, idx) => {
+        if (elem.newTask) return <NewTask key={idx} />;
+        if (elem.active) return <AcceptedTask key={idx} />;
+        if (elem.completed) return <CompletedTask key={idx} />;
+        if (elem.failed) return <FailedTask key={idx} />;
+      })}
     </div>
   );
 };
